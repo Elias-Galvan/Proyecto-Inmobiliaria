e5efdd807729647cb30696916229d855b7d66cdb
Elias Galvan <elias.alberto.galvan@gmail.com>
2023-04-07 15:06:53 -0300

    Se creo rama para propiedades_destacadas

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,9 +10,9 @@ function OffcanvasExample() {
     return (
       <>
         {['l,xxl'].map((expand) => (
-          <Navbar key={expand} bg="transparent" expand={expand} className="mb-3">
+          <Navbar key={expand} bg="transparent" expand={expand} className="mb-3" >
             <Container fluid>
-              <Navbar.Brand href="#"><img src={iconoHeader} alt="" /></Navbar.Brand>
+              <Navbar.Brand href="/"><img src={iconoHeader} alt="" /></Navbar.Brand>
               <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
               <Navbar.Offcanvas
                 id={`offcanvasNavbar-expand-${expand}`}
@@ -20,11 +20,11 @@ function OffcanvasExample() {
                 placement="end"
               >
                 <Offcanvas.Header closeButton>
-                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`} bg="light">
                     Menu
                   </Offcanvas.Title>
                 </Offcanvas.Header>
-                <Offcanvas.Body>
+                <Offcanvas.Body >
                   <Nav className="justify-content-end flex-grow-1 pe-3">
                     <Nav.Link href="#action1">Propiedades</Nav.Link>
                     <Nav.Link href="#action2">Emprendimientos</Nav.Link>
