b5be602a46afdad4ce3140d48fe5c8bd8a11f59e
Elias Galvan <elias.alberto.galvan@gmail.com>
2023-05-07 20:47:57 -0300

    07/05

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import "../assets/css/Actividades.css";
 
+
 export default function Card({
-  dir,
   imgUrl,
   titulo,
-  adrress,
   descripcion,
-  datos,
+
+
 }) {
   return (
-    <div className="card">
-      <img src={imgUrl} alt="" />
+    <div className="card text-center bg-dark">
+      <div className="overflow">
+      <img src={imgUrl} alt="" className="card-img-top" />
+      </div>
       <div className="card-body">
-        <h4 className="card-title">{titulo}</h4>
-        <p className="card-text text-secondary">asdasdasdasdasdasdasdasd</p>
+        <h4 className="card-title ">{titulo}</h4>
+        <p className="card-text text-secondary">{descripcion}</p>
+        <a
+          href="#!"
+          target="_blank"
+          className="btn btn-secondary rounded-0"
+          rel="noreferrer"
+          >Reserva una clase</a>
+        
       </div>
     </div>
   );
--- src/containers/Actividades.jsx
+++ src/containers/Actividades.jsx
@@ -1,17 +1,58 @@
 import React from "react";
 import Card from "../components/Card";
-import img3 from "../assets/statics/img3.webp";
+import img3 from "../assets/statics/img2.jpg";
+import img2 from "../assets/statics/img3.jpg";
+import img1 from "../assets/statics/img1.jpg";
 import "../assets/css/Actividades.css";
 
+
+const cards = [
+  {
+    id: 1,
+    title: "Musculacion",
+    desc: "sdfsdfsdfsd",
+    img: img1,
+  },
+  {
+    id: 2,
+    title: "Cinta",
+    desc: "sdfsdfsdfsdf",
+    img: img2,
+  },
+  {
+    id: 3,
+    title: "Boxeo",
+    desc:"dfstgdfgdfgdfg",
+    img: img3,
+  },
+];
 export default function Actividades() {
   return (
     <>
-    <div className="container-act">
-    <h2>Actividadecvbdfgs</h2>
+    <div className="actividades">
+    <div className="container justify-content-center align-items-center">
+    <h2>Actividades</h2>
       <hr className="linea" />
+      <div className="row">
+      {cards.map(({ title, desc, img, id }) => (
+          <div className="col-md-4" key={id}>
+            <Card imgUrl={img} titulo={title} descripcion={desc} />
+          </div>
+        ))}
+     
+
+
+      
+
+
+      </div>
+      
 
 
     </div>
+
+    </div>
+    
      
     </>
   );
