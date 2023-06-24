c0be4fbb64073e78a3b327b3b1b0507d0ba4e93e
SebastianAllende91 <82298580+SebastianAllende91@users.noreply.github.com>
2023-06-24 17:18:34 -0300

    Funcionalidad agregada al servicio para alta de productos y refactorizacion de vista de la tienda

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../../assets/statics/proteina.jpg";
 import Card from "./components/Card/Card";
-import protein from "../../assets/statics/protein_mix.png";
 import Filter from "./components/Filters/Filter";
 import "../../assets/css/Productos.css";
+import { useNavigate } from "react-router-dom";
+import { getProductService } from "../../services/getProductService";
 
 const Productos = () => {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  const getData = async () => {
+    const getProductData = await getProductService();
+    setProducts(getProductData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const handleSearch = (value) => {
     console.log(value);
   };
@@ -17,13 +30,19 @@ const Productos = () => {
           <img src={Banner} alt="icon banner" />
         </h1>
       </div>
+      <div className="btn-add-product">
+        <button
+          className="button-64"
+          onClick={() => navigate("/agregar-producto")}
+        >
+          <span className="text">Agregar producto</span>
+        </button>
+      </div>
       <div className="containerFiltros">
         <Filter handleSearch={handleSearch} />
       </div>
       <div className="containerCard">
-        {[1, 2, 3, 4].map((elem) => (
-          <Card key={elem} srcImage={protein} title="Proteina" />
-        ))}
+        {products && products.map((elem) => <Card key={elem.id} {...elem} />)}
       </div>
     </div>
   );
